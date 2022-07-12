f7ab6bde2129776fa2f32efb9abef4caea891457
Be Mai <bemai@gmail.com>
2022-07-12 16:52:35 +0700

    load data

--- src/Main/DataPost.jsx
+++ src/Main/DataPost.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { AppBar, Box, Grid, Link, Toolbar, Typography } from '@mui/material'
 import { Container } from '@mui/system'
+import Header from '../Components/Header'
+import TopPost from '../Components/TopPost'
 
 const DataPost = () => {
   const [mainPosts, setMainPosts] = useState([])
+  const [mainTopPosts, setTopMainPosts] = useState([])
+  const [mainCatePosts, setCatePosts] = useState([])
+  const [MorePosts, setMorePosts] = useState([])
   useEffect(() => {
     const url = `https://crawlassk.herokuapp.com/`
 
@@ -13,36 +18,20 @@ const DataPost = () => {
         title: item.title,
         link: item.link,
         image: item.image,
+        imagetop: item.imageTop,
       }))
-      setMainPosts(data.slice(5))
+      setMainPosts(data.slice(16))
+      setTopMainPosts(data.slice(4, 5))
+      setCatePosts(data.slice(6, 9))
+      setMorePosts(data.slice(9, 16))
     })
   }, [])
   return (
-    <>
-      <AppBar position="static" sx={{ background: '#fff', borderBottom: '1px solid #ede7f6', height: 50 }}>
-        <Container maxWidth="lg">
-          <Toolbar to="/">
-            <Link>
-              <img src="https://static-znews.zadn.vn/images/logo-zing-home.svg" alt="" height={'30px'} />
-            </Link>
-          </Toolbar>
-        </Container>
-      </AppBar>
-      <Box maxWidth="1000px" m="auto" p={'5rem'}>
-        {mainPosts.map((item) => (
-          <a href={item.link} key={item.title}>
-            <Grid container spacing={2} sx={{ display: 'flex', borderBottom: '1px solid #ede7f6', padding: '10px' }}>
-              <Grid item xs={8}>
-                <Typography>{item.title}</Typography>
-              </Grid>
-              <Grid item xs={4}>
-                <img src={item.image} alt="" height={'100px'} width={'50%'} />
-              </Grid>
-            </Grid>
-          </a>
-        ))}
-      </Box>
-    </>
+    <Box sx={{ backgroundColor: 'rgb(247, 247, 247)' }}>
+      <Header />
+
+      <TopPost toppost={mainTopPosts} catepost={mainCatePosts} maincate={mainPosts} mainmore={MorePosts} />
+    </Box>
   )
 }
 
